cc15f7c89d31df50d7b504a9141ef8289f566cea
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-22 17:11:38 -0600

    Add skips

--- test/turn-test.js
+++ test/turn-test.js
@@ -1,9 +1,19 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-//
-// const Turns = require('../src/turns');
-//
-// describe('Turns', function() {
-//
-//   it('should be a function')
-// })
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/turns');
+
+describe('Turns', function() {
+
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Turn', function () {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+
+})
