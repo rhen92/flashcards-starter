431390d54e90e5c4cd777f6f4a8979240d55d0f3
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-22 17:59:55 -0600

    Add 3 it blocks

--- test/turn-test.js
+++ test/turn-test.js
@@ -1,7 +1,8 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Turn = require('../src/turns');
+const Turn = require('../src/turn');
+const Card = require('../src/Card');
 
 describe('Turns', function() {
 
@@ -15,5 +16,23 @@ describe('Turns', function() {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
+  it.skip('should store a user\'s guess', function () {
+    const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const turn = new Turn ('Atlantic', card);
+    expect(turn.guess).to.equal('Atlantic');
+  });
 
+  it.skip('should store current card in play', function () {
+    const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const turn = new Turn ('Atlantic', card);
+    expect(turn.currentCard).to.equal(card);
+  });
+
+  it.skip('should return guess', function () {
+    const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const turn = new Turn ('Atlantic', card);
+
+    const guess1 = turn.returnGuess();
+    expect(guess1).to.equal('Atlantic');
+  });
 })
