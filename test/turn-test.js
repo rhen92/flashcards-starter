387d09e7046d3de5453873e0c24be16d4abe0e26
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-22 20:51:00 -0600

    Remove skips and edit it blocks

--- test/turn-test.js
+++ test/turn-test.js
@@ -6,29 +6,29 @@ const Card = require('../src/Card');
 
 describe('Turns', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it.skip('should be an instance of Turn', function () {
+  it('should be an instance of Turn', function () {
     const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
-  it.skip('should store a user\'s guess', function () {
+  it('should store a user\'s guess', function () {
     const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
     const turn = new Turn ('Atlantic', card);
     expect(turn.guess).to.equal('Atlantic');
   });
 
-  it.skip('should store current card in play', function () {
+  it('should store current card in play', function () {
     const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
     const turn = new Turn ('Atlantic', card);
     expect(turn.currentCard).to.equal(card);
   });
 
-  it.skip('should return guess', function () {
+  it('should return guess', function () {
     const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
     const turn = new Turn ('Atlantic', card);
 
@@ -36,29 +36,30 @@ describe('Turns', function() {
     expect(guess1).to.equal('Atlantic');
   });
 
-  it.skip('should return card', function () {
+  it('should return card', function () {
     const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
     const turn = new Turn ('Atlantic', card);
-    expect(turn.returnCard().to.equal(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific'));
+
+    const card1 = turn.returnCard();
+    expect(card1).to.equal(card);
   });
 
-  it.skip('should evaluate guess', function () {
+  it('should evaluate guess', function () {
     const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
     const turn = new Turn ('Pacific', card);
-    const guess1 = turn.returnGuess();
-    expect(turn.evaluateGuess(guess1).to.equal('Pacific'));
+    const guess1 = turn.evaluateGuess();
+    expect(guess1).to.equal(true);
   });
 
-  it.skip('should give feedback', function() {
+  it('should give feedback', function() {
     const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
     const turn1 = new Turn ('Atlantic', card);
     const turn2 = new Turn ('Pacific', card);
-    const guess1 = turn1.returnGuess();
-    const guess2 = turn2.returnGuess();
-    
-    turn1.evaluateGuess(guess1);
-    expect(turn1.giveFeedback().to.equal('Incorrect!'));
-    turn2.evaluateGuess(guess2);
-    expect(turn2.giveFeedback().to.equal('Correct'));
+    const guess1 = turn1.giveFeedback();
+    const guess2 = turn2.giveFeedback();
+
+    expect(guess1).to.equal('Incorrect!');
+
+    expect(guess2).to.equal('Correct!');
   })
 })
