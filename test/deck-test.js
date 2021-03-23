2015a21bc82379d6e4a3e2cb2d12c07d945e0fba
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-23 17:36:23 -0600

    Add last it block

--- test/deck-test.js
+++ test/deck-test.js
@@ -22,5 +22,14 @@ describe('Deck', function() {
     const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
     const deck = new Deck([card1, card2, card3]);
     expect(deck.totalCards).to.equal([card1, card2, card3]);
-  })
+  });
+
+  it.skip('should count cards', function() {
+    //deck.countCards() => 3
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.CountCards().to.equal(3));
+  });
 })
