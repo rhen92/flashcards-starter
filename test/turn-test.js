cee82ca99f386ee34c379731bf1489df11cd66f4
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-22 20:05:29 -0600

    Add 3 it blocks

--- test/turn-test.js
+++ test/turn-test.js
@@ -35,4 +35,30 @@ describe('Turns', function() {
     const guess1 = turn.returnGuess();
     expect(guess1).to.equal('Atlantic');
   });
+
+  it.skip('should return card', function () {
+    const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const turn = new Turn ('Atlantic', card);
+    expect(turn.returnCard().to.equal(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific'));
+  });
+
+  it.skip('should evaluate guess', function () {
+    const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const turn = new Turn ('Pacific', card);
+    const guess1 = turn.returnGuess();
+    expect(turn.evaluateGuess(guess1).to.equal('Pacific'));
+  });
+
+  it.skip('should give feedback', function() {
+    const card = new Card(1, 'Name the world\'s largest ocean', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const turn1 = new Turn ('Atlantic', card);
+    const turn2 = new Turn ('Pacific', card);
+    const guess1 = turn1.returnGuess();
+    const guess2 = turn2.returnGuess();
+    
+    turn1.evaluateGuess(guess1);
+    expect(turn1.giveFeedback().to.equal('Incorrect!'));
+    turn2.evaluateGuess(guess2);
+    expect(turn2.giveFeedback().to.equal('Correct'));
+  })
 })
