afcf0c5baf7e0f882de64b7f6cebea314498841e
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-23 17:24:40 -0600

    Add 3 it blocks

---
+++ test/deck-test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+  it.skip('should be a function', function() {
+    const deck = new Deck;
+    expect(Deck).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it.skip('should hold all of the cards', function() {
+    //this.totalCards = totalCards
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.totalCards).to.equal([card1, card2, card3]);
+  })
+})
