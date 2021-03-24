a6f34b1ecb0921af0848b28e6870d9abfb932756
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-23 20:37:28 -0600

    Work stopping point

---
+++ test/round-test.js
@@ -0,0 +1,69 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/round');
+const Card = require('../src/Card');
+const Deck = require('../src/deck');
+const Turn = require('../src/turn');
+
+describe('Round', function() {
+
+  it.skip('should be a function', function() {
+    const round = new Round;
+    expect(Round).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it.skip('should contain the deck of cards', function() {
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck1 = new Deck([card1, card2, card3]);
+    const round = new Round(deck1);
+    expect(round.deck).to.deep.equal(deck1);
+  })
+
+  it.skip('should return current card being played', function() {
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck1 = new Deck([card1, card2, card3]);
+    const round = new Round(deck1);
+    const currentCard = round.returnCurrentCard();
+    expect(currentCard).to.equal(card1);
+  });
+
+  it.skip('should start at 0 turns by default', function() {
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck1 = new Deck([card1, card2, card3]);
+    const round = new Round(deck1);
+    expect(round.turns).to.equal(0);
+  });
+
+  it.skip('should start with no incorrect guesses', function() {
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck1 = new Deck([card1, card2, card3]);
+    const round = new Round(deck1);
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+
+  it.skip('should increase turns when a turn is taken', function() {
+    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    const deck1 = new Deck([card1, card2, card3]);
+    const round = new Round(deck1);
+    const turn = new Turn('Atlantic', card);
+    expect(round.takeTurn().to.increase(round.turns).by(1));
+  });
+
+  it.skip('should ') 
+})
