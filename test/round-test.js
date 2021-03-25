cb56ca494525ac96d99bc5740a2b706ace78b46f
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-24 19:45:28 -0600

    Remove skips and fix it blocks

--- test/round-test.js
+++ test/round-test.js
@@ -7,112 +7,83 @@ const Deck = require('../src/deck');
 const Turn = require('../src/turn');
 
 describe('Round', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck1;
+  let round;
+  beforeEach(function() {
+    card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
+    card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
+    card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
+    deck1 = new Deck([card1, card2, card3]);
+    round = new Round(deck1);
+  });
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     const round = new Round;
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Round', function() {
+  it('should be an instance of Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it.skip('should contain the deck of cards', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
+  it('should contain the deck of cards', function() {
     expect(round.deck).to.deep.equal(deck1);
   })
 
-  it.skip('should return current card being played', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
-    const currentCard = round.returnCurrentCard();
+  it('should return current card being played', function() {
+    const turn = new Turn('Atlantic', card1);
+    const currentCard = round.returnCurrentCard(turn);
     expect(currentCard).to.equal(card1);
   });
 
-  it.skip('should start at 0 turns by default', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
+  it('should start at 0 turns by default', function() {
     expect(round.turns).to.equal(0);
   });
 
-  it.skip('should start with no incorrect guesses', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
+  it('should start with no incorrect guesses', function() {
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
 
-  it.skip('should increase turns when a turn is taken', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
-    const turn = new Turn('Atlantic', card);
-    expect(round.takeTurn().to.increase(round.turns).by(1));
+  it('should increase turns when a turn is taken', function() {
+    const turn = new Turn('Atlantic', card1);
+    const guess1 = round.takeTurn(turn);
+    expect(round.turns).to.equal(1);
   });
 
-  it.skip('should return negative feedback after taking a turn if incorrect', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
-    const turn = new Turn('Atlantic', card);
+  it('should return negative feedback after taking a turn if incorrect', function() {
+    const turn = new Turn('Atlantic', card1);
     turn.evaluateGuess();
     turn.giveFeedback();
-    expect(round.takeTurn().to.equal('Incorrect!'));
+    expect(round.takeTurn(turn)).to.equal('Incorrect!');
   });
 
-  it.skip('should return positive feedback after taking a turn if correct', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
-    const turn = new Turn('Pacific', card);
+  it('should return positive feedback after taking a turn if correct', function() {
+    const turn = new Turn('Pacific', card1);
     turn.evaluateGuess();
     turn.giveFeedback();
-    expect(round.takeTurn().to.equal('Correct!'));
+    expect(round.takeTurn(turn)).to.equal('Correct!');
   })
 
-  it.skip('should evaluate and record incorrect guesses', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
-    const turn = new Turn('Atlantic', card);
+  it('should record incorrect guesses', function() {
+    const turn = new Turn('Atlantic', card1);
     turn.evaluateGuess();
     turn.giveFeedback();
-    round.takeTurn();
-    expect(round.incorrectGuesses).to.deep.equal('Atlantic');
+    round.takeTurn(turn);
+    expect(round.incorrectGuesses).to.deep.equal([1]);
   });
 
-  it.skip('should evaluate and move on to next card if correct guess was given', function() {
-    const card1 = new Card(1, 'Name the world\'s largest ocean.', ['Pacific', 'Atlantic', 'Artic'], 'Pacific');
-    const card2 = new Card(2, 'How many taste buds does the average human tongue have?', ['3000', '5000', '10,000', '15,000'], '10,000');
-    const card3 = new Card(3, 'What is the largest big cat?', ['Lion', 'Tiger', 'Cheetah'], 'Tiger');
-    const deck1 = new Deck([card1, card2, card3]);
-    const round = new Round(deck1);
-    const turn = new Turn('Pacific', card);
+  it('should move on to next card if correct guess was given', function() {
+    const turn = new Turn('Pacific', card1);
     turn.evaluateGuess();
     turn.giveFeedback();
-    round.takeTurn();
-    expect(round.returnCurrentCard().to.equal(card2));
+    round.takeTurn(turn);
+    const turn2 = new Turn('3000', card2)
+    const changeCard = round.returnCurrentCard(turn2);
+    expect(changeCard).to.equal(card2);
   });
 
 
